b2c6aec52b7febb0f21bdf43d6f5d61f30b33df5
sama siri <sirisama4@gmail.com>
2024-12-28 13:43:17 +0530

    updated to button format

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function fun1(){
-    const dis=document.getElementById("display");
-    dis.textContent="Index page";
+    window.location.href="./home.html";
+}
+function fun2(){
+    window.location.href="./index.html";
+
+}
+function fun3(){
+    window.location.href="./about.html";
+
 }
